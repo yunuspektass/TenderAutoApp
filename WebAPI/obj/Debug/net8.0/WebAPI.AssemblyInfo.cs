9eed0ac8cc3853fbcaefc454bb6a827e22b0bb19
Yunus Pektaş <145374270+yunuspektass@users.noreply.github.com>
2024-10-20 22:11:57 +0300

    dockerfile yml dosyası güncellendi

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004d1fa84ed15dc26f600e1ba8a50a36d2d34f0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58a381725af0b451af88e69c25d3b921fef4bc46")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
