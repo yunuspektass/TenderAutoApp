146d5efa54d95ce2825a2e310db3e0274191bd8c
Yunus Pektaş <145374270+yunuspektass@users.noreply.github.com>
2024-10-20 16:37:10 +0300

    app.yml dosyası eklendi

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656834fd701c23772e7e208b792ff12bd9c2c240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8921668acc0fbcc8cfaa5a159bbbdb5a0c9abc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
